19d5999962c140dd197ed8455cc1db0d6ca8b2c4
Udo Brocker <udo.brocker@web.de>
2019-02-16 18:45:21 +0100

    geändert:       src/acceptsocket.cpp 			bei Fehlern am Soccket keinen Trap sondern eine 			Warnung ausgeben

--- src/acceptsocket.cpp
+++ src/acceptsocket.cpp
@@ -1,6 +1,6 @@
 #include "config.h"
 #include "trap.h"
-
+#include "util.h"
 #include "acceptsocket.h"
 
 #include <stdint.h>
@@ -37,27 +37,34 @@ AcceptSocket::AcceptSocket(string port)
 	gai_accept_address = 0;
 
 	if((fd = socket(AF_INET6, SOCK_STREAM | SOCK_CLOEXEC, 0)) < 0)
-		throw(trap("AcceptSocket: cannot create accept socket"));
+		Util::vlog("AcceptSocket: cannot create accept socket, port %s", port.c_str());
+//		throw(trap("AcceptSocket: cannot create accept socket"));
 
 	if(setsockopt(fd, SOL_SOCKET, SO_LINGER, &so_linger, sizeof(so_linger)))
-		throw(trap("AcceptSocket: cannot set linger on accept socket"));
+		Util::vlog("AcceptSocket: cannot set linger on accept socket, port %s", port.c_str());
+//		throw(trap("AcceptSocket: cannot set linger on accept socket"));
 
 	rv = 1;
-
 	if(setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &rv, sizeof(rv)))
-		throw(trap("AcceptSocket: cannot set reuseaddr on accept socket"));
+		Util::vlog("AcceptSocket: cannot set reuseaddr on accept socket, port %s", port.c_str());
+//		throw(trap("AcceptSocket: cannot set reuseaddr on accept socket"));
 
 	if((rv = getaddrinfo(0, port.c_str(), &gai_accept_hints, &gai_accept_address)))
-		throw(trap(string("AcceptSocket: cannot get address info: ") + gai_strerror(rv)));
+		Util::vlog("AcceptSocket: cannot get address info:i %s, port %s", gai_strerror(rv), port.c_str());
+//		throw(trap(string("AcceptSocket: cannot get address info: ") + gai_strerror(rv)));
 
 	if(!gai_accept_address)
-		throw(trap(string("AcceptSocket: cannot get address info: ") + gai_strerror(rv)));
+		Util::vlog("AcceptSocket: cannot get address info:i %s, port %s", gai_strerror(rv), port.c_str());
+//		throw(trap(string("AcceptSocket: cannot get address info: ") + gai_strerror(rv)));
 
 	if(bind(fd, gai_accept_address->ai_addr, gai_accept_address->ai_addrlen))
-		throw(trap("AcceptSocket: cannot bind accept socket"));
+		Util::vlog("AcceptSocket: cannot bind accept socket, port %s",port.c_str());
+//		throw(trap("AcceptSocket: cannot bind accept socket"));
 
 	if(listen(fd, 4))
-		throw(trap("AcceptSocket: cannot listen on accept socket"));
+		Util::vlog("AcceptSocket: cannot listen on accept socket, port %s",port.c_str());
+//		throw(trap("AcceptSocket: cannot listen on accept socket"));
+
 }
 
 AcceptSocket::~AcceptSocket()
